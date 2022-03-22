c3f1ab80b2f855b67bc8e75a347aa9d04a116995
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-03-22 11:32:30 +0600

    updated

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { addToDb } from '../../uttilities/local-storage';
+import { addToDb, removeFromId } from '../../uttilities/local-storage';
 import './Cosmetic.css'
 const Cosmetic = (props) => {
     const { id, name, price } = props.cosmetic;
     const addCart = (Id) => {
         addToDb(id)
+    };
+    // work of delete button 
+    const RemoveFromCart = id => {
+        removeFromId(id);
     }
     const addToCart = () => addCart(id);
     return (
@@ -14,6 +18,7 @@ const Cosmetic = (props) => {
             <p>Price:{price}</p>
             <button onClick={addToCart}>add to cart</button>
             <button onClick={() => addCart(id)}>add to cart with short cut</button>
+            <button onClick={() => RemoveFromCart(id)}>delete</button>
         </div>
     );
 };
--- src/uttilities/local-storage.js
+++ src/uttilities/local-storage.js
@@ -1,16 +1,58 @@
 // Here, using local storage to manage cart data 
+// const addToDb = (id) => {
+//     const quantity = localStorage.getItem(id);
+//     if (quantity == '1') {
+//         console.log('Already exist');
+//         const newQuantity = parseInt(quantity) + 1;
+//         localStorage.setItem(id, newQuantity);
+
+//     }
+//     else {
+//         console.log('new item');
+//         localStorage.setItem(id, 1)
+//     }
+
+// }
+// export { addToDb }
 const addToDb = (id) => {
-    const quantity = localStorage.getItem(id);
-    if (quantity == '1') {
-        console.log('Already exist');
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+    let shoppingCart = {};
+
+    // get shopping cart 
+    const storeCart = localStorage.getItem('shopping-cart');
+    if (storeCart) {
+        shoppingCart = JSON.parse(storeCart)
+    }
+    else {
+
+    }
+
+    // add quantity 
+    const quantity = shoppingCart[id];
+    if (quantity) {
+
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+
+
+
 
     }
     else {
-        console.log('new item');
-        localStorage.setItem(id, 1)
+        shoppingCart[id] = 1;
     }
-
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
-export { addToDb }
+const removeFromId = (id) => {
+    const storeCart = localStorage.getItem('shopping-cart');
+    if (storeCart) {
+        const shoppingCart = JSON.parse(storeCart)
+        if (id in shoppingCart) {
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+        }
+    }
+}
+export {
+    addToDb,
+    removeFromId
+}
